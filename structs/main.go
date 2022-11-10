718b32c40e64b2ac6aa1fca1742c9745a39da001
nguyentruyen98 <haohaothieugia@gmail.com>
2022-11-10 22:05:55 +0700

    pass by value

--- structs/main.go
+++ structs/main.go
@@ -2,15 +2,34 @@ package main
 
 import "fmt"
 
+type contractInfo struct {
+	email   string
+	zipCode int
+}
+
 type person struct {
 	lastName  string
 	firstName string
+	contractInfo
 }
 
 func main() {
-	var alex person
-	alex.firstName = "Truyen"
-	alex.lastName = "Nguyen"
+	jim := person{firstName: "Jim", lastName: "Party", contractInfo: contractInfo{email: "jim@gmail.com", zipCode: 70000}}
+	test := 123
+	jimPointer := &jim
+	fmt.Println(&test)
 
-	fmt.Printf("%+v", alex)
+	jimPointer.updateName("Truyen")
+	// jim.updateName("Truyen")
+	jim.print()
+}
+
+func (pointerToPersion *person) updateName(newFirstName string) {
+	fmt.Println(pointerToPersion)
+	(*pointerToPersion).firstName = newFirstName
+
+}
+
+func (p person) print() {
+	fmt.Printf("%+v", p)
 }
