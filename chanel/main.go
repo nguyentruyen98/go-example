1a04a4af0abd51a645cbfe0b521863fc087d1d84
nguyentruyen98 <haohaothieugia@gmail.com>
2022-12-09 01:26:10 +0700

    chanel

--- chanel/main.go
+++ chanel/main.go
@@ -7,23 +7,42 @@ import (
 
 func main() {
 	links := []string{
+		"https://positivethinking.udemy.com",
 		"https://www.facebook.com",
 		"https://www.google.com",
-		"https://positivethinking.udemy.com",
 	}
 
+	c := make(chan string)
+
 	for _, link := range links {
-		checkLink(link)
+		go checkLink(link, c)
+
+	}
+	// for i := 0; i < len(links); i++ {
+	// 	fmt.Println(<-c)
+
+	// }
+	// for l := range c {
+	// 	go func(l string) {
+	// 		time.Sleep(time.Second * 5)
+	// 		checkLink(l, c)
+	// 	}(l)
+	// }
+	for {
+		fmt.Println(<-c)
 	}
 
 }
 
-func checkLink(link string) {
+func checkLink(link string, c chan string) {
 	_, err := http.Get(link)
 	if err != nil {
 		fmt.Println(link, "might be down!")
+		// c <- "Might be down I think"
+		c <- link
 		return
 	}
 
 	fmt.Println(link, "is up!")
+	c <- link
 }
